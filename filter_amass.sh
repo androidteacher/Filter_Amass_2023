19017968757857113fe8ed81aa05e37fad4b58c9
CyberLessons101 <joshbeck2006+androidclass@gmail.com>
2023-11-24 10:55:31 -0600

    Update filter_amass.sh
    test

--- filter_amass.sh
+++ filter_amass.sh
@@ -1,1 +1,6 @@
-grep '\( FQDN \)$' | awk '{print $(NF-2)}'
+#!/bin/bash
+
+cat $1 | grep -Eo '([a-zA-Z0-9.-]+\.com) \(FQDN\)' | awk '{print $1}' | anew out.txt
+cat $1 | egrep '\(FQDN\)$' | awk '{print $(NF-1)}' | anew out.txt
+
+
